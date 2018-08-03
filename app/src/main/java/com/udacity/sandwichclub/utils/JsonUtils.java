0be9e2a5d8cd105f70a858b9d728ccdb37e98d77
Anmol Manchanda <manchandaanmol@icloud.com>
2018-08-03 16:40:39 +0530

    Update JsonUtils.java

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -10,18 +10,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
+    private final static String NAME_CODE = "name";
+    private final static String MAIN_NAME_CODE = "mainName";
+    private final static String ALSO_KNOWN_AS_CODE = "alsoKnownAs";
+    private final static String PLACE_OF_ORIGIN_CODE = "placeOfOrigin";
+    private final static String DESCRIPTION_CODE = "description";
+    private final static String IMAGE_CODE = "image";
+    private final static String INGREDIENTS_CODE = "ingredients";
 
     public static Sandwich parseSandwichJson(String json) {
         try {
             JSONObject mainJsonObject = new JSONObject( json );
-            JSONObject name = mainJsonObject.getJSONObject( "name" );
-            String mainName = name.getString( "mainName" );
-            JSONArray alsoKnownAsJsonArray = name.getJSONArray( "alsoKnownAs" );
+            JSONObject name = mainJsonObject.optJSONObject( NAME_CODE );
+            String mainName = name.optString( MAIN_NAME_CODE );
+            JSONArray alsoKnownAsJsonArray = name.optJSONArray( ALSO_KNOWN_AS_CODE );
             List <String> alsoKnownAs = convertToListFromJsonArray( alsoKnownAsJsonArray );
-            String placeOfOrigin = mainJsonObject.optString( "placeOfOrigin" );
-            String description = mainJsonObject.getString( "description" );
-            String image = mainJsonObject.getString( "image" );
-            JSONArray ingredientsJsonArray = mainJsonObject.getJSONArray( "ingredients" );
+            String placeOfOrigin = mainJsonObject.optString( PLACE_OF_ORIGIN_CODE );
+            String description = mainJsonObject.optString( DESCRIPTION_CODE );
+            String image = mainJsonObject.optString( IMAGE_CODE );
+            JSONArray ingredientsJsonArray = mainJsonObject.optJSONArray( INGREDIENTS_CODE );
             List <String> ingredients = convertToListFromJsonArray( ingredientsJsonArray );
             return new Sandwich( mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients );
         } catch (JSONException e) {
@@ -34,7 +41,7 @@ public class JsonUtils {
         List <String> list = new ArrayList <>( jsonArray.length() );
 
         for (int i = 0; i < jsonArray.length(); i++) {
-            list.add( jsonArray.getString( i ) );
+            list.add( jsonArray.optString( i ) );
         }
 
         return list;
